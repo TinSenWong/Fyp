363c3ad8213f123032bc44a5b49f1e1c64fdb7f9
WAN Shing Wah <170070111@stu.vtc.edu.hk>
2019-04-24 02:39:56 +0800

    Lv16 not work issue fix

--- WebContent/GamePlay.php
+++ WebContent/GamePlay.php
@@ -47,6 +47,7 @@ if (!isset($_SESSION["userID"])) {
     <script src="assets/canvas/Level13.js"></script>
     <script src="assets/canvas/Level14.js"></script>
     <script src="assets/canvas/Level15.js"></script>
+    <script src="assets/canvas/Level16.js"></script>
 
     <script src="assets/canvas/index.js"></script>
     <script src="assets/canvas/game1.js"></script>
