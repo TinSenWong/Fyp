acef904fded5a4a7f14de12c5442329e99c38c8b
Andy2682 <andy_2682@yahoo.com.hk>
2019-04-23 18:33:25 +0800

    up

--- WebContent/ResetPW.php
+++ WebContent/ResetPW.php
@@ -7,7 +7,6 @@
  */
 $errors = array();
 $errorDIV = null;
-<<<<<<< HEAD
 $SQL_servername = "localhost";
 $SQL_username = "root";
 $SQL_password = "";
@@ -118,51 +117,6 @@ if (!isset($_SESSION["userID"])) {
                 $errorDIV .= "<H2 style='color:red;'> $error </H2>";
             }
             $errorDIV .= "</div>";
-=======
-if (isset($_POST['submit'])){
-
-    if ($_POST['NewPassword1'] != $_POST['NewPassword2']) {
-        array_push($errors, '<p>Your passwords do not match.</p>');
-    }elseif ($_POST['OldPassword'] != $_POST['NewPassword1']) {
-        array_push($errors, '<p>The new password cannot be the same as the old password.</p>');
-    }
-    if (count($errors) == 0) {
-        $SQL_servername = "localhost";
-        $SQL_username = "root";
-        $SQL_password = "";
-        $SQL_database = "fyp";
-        session_start();
-        $conn = @mysqli_connect($SQL_servername, $SQL_username, $SQL_password,$SQL_database)
-        or die ("<p align='center'>Server Connection Failed</p>");
-
-        $op = $conn->real_escape_string($_POST['OldPassword']);
-        $np = $conn->real_escape_string($_POST['NewPassword1']);
-        $np2 = $conn->real_escape_string($_POST['NewPassword2']);
-        $op = md5($op);
-        $result = mysqli_query($conn,"select * from userinfo where UserID = '{$_SESSION["userID"]}' and password = '$op'")or die ();
-        $row=mysqli_fetch_array($result);
-        if (mysqli_num_rows($result)>0){
-            $np = md5($np);
-            $result = mysqli_query($conn,"UPDATE userinfo SET password='$np' WHERE UserID = '{$_SESSION["userID"]}'")or die ();
-            $correct = true;
-            $sec = "10";
-            header("Refresh:$sec;url=LoginPage.php");
-        }
-
-    } else {
-        $errorDIV = "<div class='error'>";
-        foreach ($errors as $error) {
-            $errorDIV .= "<H2 style='color:red;'> $error </H2>";
-        }
-        $errorDIV .= "</div>";
-    }
-}
-if (isset($vkey)) {
-    $result = mysqli_query($conn, "select * from userinfo where vkey = '$vkey'") or die ();
-    if (mysqli_num_rows($result) > 0) {
-        while ($values = mysqli_fetch_assoc($result)) {
-            $_SESSION["userID"] = $values['UserID'];
->>>>>>> 32cf7d167b4f3051bf40e6a55c2535c145513902
         }
     }
 }
@@ -187,7 +141,6 @@ if (isset($vkey)) {
     <div class="container">
         <br/>
         <br/>
-<<<<<<< HEAD
 
         <?php
         if (isset($correct)) {
@@ -262,52 +215,12 @@ if (isset($vkey)) {
                         <?php
                     }
                     ?>
-=======
-        <h2 class="text-center" id="title">Welcome</h2>
-        <?php
-        if (isset($correct)){
-            echo " <H1 align='center'>
-              Please login again.<br /></H4>
-              <H2 align='center'>Page will redirect to login page in 10 sec.</H2></p>";
-        }else {
-            ?>
-            <hr>
-            <div class="row">
-                <div class="col-md-5">
-                    <form role="form" method="post" action="">
-                        <fieldset>
-                            <p class="text-uppercase pull-center"> Login.</p>
-                            <div class="form-group">
-                                <input type="password" name="OldPassword" id="OldPassword" class="form-control input-lg"
-                                       placeholder="OldPassword" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="password" name="NewPassword1" id="NewPassword1"
-                                       class="form-control input-lg" placeholder="NewPassword1" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="password" name="NewPassword2" id="NewPassword2"
-                                       class="form-control input-lg" placeholder="NewPassword2" required>
-                            </div>
-                            <div>
-                                <input type="submit" class="btn btn-lg btn-primary" name="submit" id="submit"
-                                       value="Reset">&nbsp
-                                <a href="LoginPage.php" class="btn btn-lg btn-primary">Sign in</a>
-
-                            </div>
-                        </fieldset>
-                    </form>
->>>>>>> 32cf7d167b4f3051bf40e6a55c2535c145513902
                 </div>
 
             </div>
             <?php
         }
-<<<<<<< HEAD
         echo $errorDIV;
-=======
-            echo $errorDIV;
->>>>>>> 32cf7d167b4f3051bf40e6a55c2535c145513902
         ?>
     </div>
 </div>
